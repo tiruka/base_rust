b5803817b7b2c22dec7e52fe55a9b50074e63ce6
tiruka <tiruka.engineer@gmail.com>
2024-10-02 09:43:48 +0900

    update for ch5

--- src/bin/chapter5.rs
+++ src/bin/chapter5.rs
@@ -1,4 +1,10 @@
-use std::{cell::UnsafeCell, mem::MaybeUninit, sync::atomic::AtomicBool, thread};
+use std::sync::atomic::Ordering::Relaxed;
+use std::{
+    cell::UnsafeCell,
+    mem::MaybeUninit,
+    sync::{atomic::AtomicBool, Arc},
+    thread,
+};
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
@@ -42,17 +48,76 @@ impl<T> Drop for Channel<T> {
     }
 }
 
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let a = Arc::new(Channel2 {
+        message: UnsafeCell::new(MaybeUninit::uninit()),
+        ready: AtomicBool::new(false),
+    });
+    (Sender { channel: a.clone() }, Receiver { channel: a })
+}
+
+struct Channel2<T> {
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
+}
+unsafe impl<T> Sync for Channel2<T> where T: Send {}
+pub struct Sender<T> {
+    channel: Arc<Channel2<T>>,
+}
+pub struct Receiver<T> {
+    channel: Arc<Channel2<T>>,
+}
+
+impl<T> Sender<T> {
+    pub fn send(self, message: T) {
+        unsafe { (*self.channel.message.get()).write(message) };
+        self.channel.ready.store(true, Relaxed);
+    }
+}
+impl<T> Receiver<T> {
+    pub fn is_ready(&self) -> bool {
+        self.channel.ready.load(Relaxed)
+    }
+    pub fn receive(self) -> T {
+        if !self.channel.ready.swap(false, Relaxed) {
+            panic!("no message available");
+        }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
+    }
+}
+impl<T> Drop for Channel2<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { self.message.get_mut().assume_init_drop() }
+        }
+    }
+}
+
 fn main() {
-    let channel = Channel::new();
+    let ch = Channel::new();
     let t = thread::current();
     thread::scope(|s| {
         s.spawn(|| {
-            channel.send("hello world!");
+            ch.send("hello world!");
             t.unpark();
         });
-        while !channel.is_ready() {
+        while !ch.is_ready() {
             thread::park();
         }
-        assert_eq!(channel.receive(), "hello world!");
+        assert_eq!(ch.receive(), "hello world!");
+    });
+
+    // Using Channel2
+    thread::scope(|s| {
+        let (sender, receiver) = channel();
+        let t = thread::current();
+        s.spawn(move || {
+            sender.send("Hello World");
+            t.unpark();
+        });
+        while !receiver.is_ready() {
+            thread::park();
+        }
+        assert_eq!(receiver.receive(), "Hello World");
     });
 }
