8506fbf25309dd965d9b3c4fed9111d0e71ea652
tiruka <tiruka.engineer@gmail.com>
2024-10-04 20:38:13 +0900

    add weak

--- src/bin/chapter6.rs
+++ src/bin/chapter6.rs
@@ -1,10 +1,10 @@
+use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 use std::{
     cell::UnsafeCell,
     ops::Deref,
     ptr::NonNull,
     sync::atomic::{fence, AtomicUsize},
 };
-
 fn main() {}
 
 struct ArcData<T> {
@@ -38,6 +38,24 @@ impl<T> Weak<T> {
     fn data(&self) -> &ArcData<T> {
         unsafe { self.ptr.as_ref() }
     }
+    fn upgrade(&self) -> Option<MyArc<T>> {
+        let mut n = self.data().data_ref_count.load(Relaxed);
+        loop {
+            if n == 0 {
+                return None;
+            }
+            assert!(n < usize::MAX / 2);
+            if let Err(e) =
+                self.data()
+                    .data_ref_count
+                    .compare_exchange_weak(n, n + 1, Relaxed, Relaxed)
+            {
+                n = e;
+                continue;
+            }
+            return Some(MyArc { weak: self.clone() });
+        }
+    }
 }
 impl<T> MyArc<T> {
     pub fn new(data: T) -> MyArc<T> {
@@ -51,6 +69,20 @@ impl<T> MyArc<T> {
             },
         }
     }
+    pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
+        if arc.weak.data().alloc_ref_count.load(Relaxed) == 1 {
+            fence(Acquire);
+            let arcdata = unsafe { arc.weak.ptr.as_mut() };
+            let option = arcdata.data.get_mut();
+            let data = option.as_mut().unwrap();
+            Some(data)
+        } else {
+            None
+        }
+    }
+    pub fn downgrade(arc: &Self) -> Weak<T> {
+        arc.weak.clone()
+    }
 }
 
 impl<T> Deref for MyArc<T> {
@@ -63,29 +95,41 @@ impl<T> Deref for MyArc<T> {
 
 impl<T> Clone for Weak<T> {
     fn clone(&self) -> Self {
-        if self
-            .data()
-            .data_ref_count
-            .fetch_add(1, std::sync::atomic::Ordering::Relaxed)
-            > usize::MAX / 2
-        {
+        if self.data().data_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
             std::process::abort();
         }
         Weak { ptr: self.ptr }
     }
 }
 
-impl<T> Drop for MyArc<T> {
+impl<T> Clone for MyArc<T> {
+    fn clone(&self) -> Self {
+        let weak = self.weak.clone();
+        if weak.data().data_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        MyArc { weak }
+    }
+}
+
+impl<T> Drop for Weak<T> {
     fn drop(&mut self) {
-        if self
-            .data()
-            .data_ref_count
-            .fetch_sub(1, std::sync::atomic::Ordering::Release)
-            == 1
-        {
-            fence(std::sync::atomic::Ordering::Acquire);
+        if self.data().data_ref_count.fetch_sub(1, Relaxed) == 1 {
+            fence(Acquire);
             unsafe {
                 drop(Box::from_raw(self.ptr.as_ptr()));
+            }
+        }
+    }
+}
+
+impl<T> Drop for MyArc<T> {
+    fn drop(&mut self) {
+        if self.weak.data().data_ref_count.fetch_sub(1, Release) == 1 {
+            fence(Acquire);
+            let ptr = self.weak.data().data.get();
+            unsafe {
+                *ptr = None;
             }
         }
     }
@@ -97,7 +141,7 @@ fn test() {
     struct DetectDrop;
     impl Drop for DetectDrop {
         fn drop(&mut self) {
-            NUM_DROPS.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            NUM_DROPS.fetch_add(1, Relaxed);
         }
     }
     let x = MyArc::new(("Hello", DetectDrop));
@@ -107,7 +151,7 @@ fn test() {
     });
     assert_eq!(y.0, "Hello");
     t.join().unwrap();
-    assert_eq!(NUM_DROPS.load(std::sync::atomic::Ordering::Relaxed), 0);
+    assert_eq!(NUM_DROPS.load(Relaxed), 0);
     drop(y);
-    assert_eq!(NUM_DROPS.load(std::sync::atomic::Ordering::Relaxed), 1);
+    assert_eq!(NUM_DROPS.load(Relaxed), 1);
 }
