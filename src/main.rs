407ffc8130cb18627facf8e4fd12c9dd7099bfd5
tiruka <tiruka.engineer@gmail.com>
2024-11-06 15:34:02 +0900

    add closure

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,47 @@ fn main() {
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation_fn: T,
+    value: Option<u32>,
+}
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation_fn: T) -> Cacher<T> {
+        Cacher {
+            calculation_fn,
+            value: None,
+        }
+    }
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation_fn)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
+    let mut expensive_closure = Cacher::new(|num| {
         println!("Calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
     // こうすることで、closureは一度呼び出されたら、その結果をletの変数にbindする。そして、もう一度呼び出されたら、中のコードを実行するのではなく、
     // bindされた結果だけを返す。従って、呼び出し回数を減らすことができる。
-    // というわけではな **ない**
+    // というわけではな **ない** -> Cacherという構造体にクロージャを持たせて、キャッシュ機構を備えさせる。
     if intensity < 25 {
-        println!("Today do {} pushes", expensive_closure(intensity));
-        println!("Next, do {} situps!", expensive_closure(intensity));
+        println!("Today do {} pushes", expensive_closure.value(intensity));
+        println!("Next, do {} situps!", expensive_closure.value(intensity));
     } else {
         if random_number == 3 {
             // 今日は休憩してください！水分補給を忘れずに！
@@ -28,7 +57,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
             println!(
                 // 今日は、{}分間走ってください！
                 "Today, run for {} minutes!",
-                expensive_closure(intensity)
+                expensive_closure.value(intensity)
             );
         }
     }
