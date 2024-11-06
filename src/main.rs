37314ee60f7d9522b144b8721191f2056704e800
tiruka <tiruka.engineer@gmail.com>
2024-11-06 13:37:39 +0900

    closureに変更、コメントを追加

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,18 @@ fn main() {
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("Calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
+    let expensive_closure = |num| {
+        println!("Calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    // こうすることで、closureは一度呼び出されたら、その結果をletの変数にbindする。そして、もう一度呼び出されたら、中のコードを実行するのではなく、
+    // bindされた結果だけを返す。従って、呼び出し回数を減らすことができる。
+    // というわけではな **ない**
     if intensity < 25 {
-        println!("Today do {} pushes", expensive_result);
-        println!("Next, do {} situps!", expensive_result);
+        println!("Today do {} pushes", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
     } else {
         if random_number == 3 {
             // 今日は休憩してください！水分補給を忘れずに！
@@ -27,7 +28,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
             println!(
                 // 今日は、{}分間走ってください！
                 "Today, run for {} minutes!",
-                expensive_result
+                expensive_closure(intensity)
             );
         }
     }
