93012fbe4b699db5e7dbd570dee2b0799df741e0
tiruka <tiruka.engineer@gmail.com>
2024-11-06 10:52:18 +0900

    on the way to refactoring

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,10 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_result = simulated_expensive_calculation(intensity);
     if intensity < 25 {
-        println!(
-            "Today do {} pushes",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
-        );
+        println!("Today do {} pushes", expensive_result);
+        println!("Next, do {} situps!", expensive_result);
     } else {
         if random_number == 3 {
             // 今日は休憩してください！水分補給を忘れずに！
@@ -32,7 +27,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
             println!(
                 // 今日は、{}分間走ってください！
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
+                expensive_result
             );
         }
     }
