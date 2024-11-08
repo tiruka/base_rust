420a7818ab53ce7b177433b9483056f0e139748f
tiruka <tiruka.engineer@gmail.com>
2024-11-08 11:33:39 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,25 @@ type Backend = NdArray;
 fn main() {
     let device = Default::default();
     // one hot from tensor/api/int.rs
-    let original_indices: Tensor<Backend, 1, Int> = Tensor::from_ints([0, 1, 2, 3], &device);
-    println!("original_indices: {}", original_indices);
+    let original_data: Tensor<Backend, 1, Int> = Tensor::from_ints([0, 1, 2, 3], &device);
+    println!("original_data: {}\n", original_data.to_data());
     // [0, 1, 2, 3]
 
     // 本来動くべきコード torch.nn.functional.one_hotに近い。
-    // let one_hot = original_indices.one_hot(4);
+    // let one_hot = original_data.one_hot(4);
     // println!("{}", one_hot.to_data());
     // [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
     // one_hotの中身を持ってきて、検証する。
-    let [num_samples] = original_indices.dims();
-    let indices: Tensor<Backend, 2, Int> = original_indices.unsqueeze();
+    let [num_samples] = original_data.dims();
+    println!("#num_samples: {}\n", &num_samples);
+    let indices: Tensor<Backend, 2, Int> = original_data.unsqueeze();
+    println!("#indices: {}\n", indices);
     let values: Tensor<Backend, 2, Int> = indices.ones_like();
-    let zeros: Tensor<Backend, 2, Int> =
-        Tensor::zeros([num_samples, num_samples], &indices.device());
-    let r = zeros.scatter(1, indices, values);
-    println!("{}", r.to_data());
+    println!("#values: {}\n", values);
+    let zeros: Tensor<Backend, 2, Int> = Tensor::zeros([4, 4], &indices.device());
+    println!("#zeros: {}\n", zeros);
+    // let r = zeros.scatter(1, indices, values);
+    // println!("#result: {}", r.to_data());
 
     // one hot from tensor/api/float.rs こっちは動く。
     // let one_hot = Tensor::<Backend, 1>::one_hot(2, 10, &device);
