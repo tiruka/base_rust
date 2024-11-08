7b6a6c32998b61ac968603f6d633e7ecf376c188
tiruka <tiruka.engineer@gmail.com>
2024-11-08 11:05:05 +0900

    add sample codes to debug one hot

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
-use burn::backend::Wgpu;
+use burn::backend::NdArray;
 use burn::tensor::{Int, Tensor};
-type Backend = Wgpu;
+type Backend = NdArray;
 
 fn main() {
     let device = Default::default();
-    let data = [[2., 3.], [4., 5.]];
-    let tensor_1 = Tensor::<Backend, 2>::from_data(data, &device);
-    let tensor_2 = Tensor::ones_like(&tensor_1);
-    println!("{}", tensor_1 + tensor_2);
-
     // one hot from tensor/api/int.rs
-    let indices: Tensor<Backend, 1, Int> = Tensor::from_ints([0, 1, 2, 3], &device);
-    let one_hot = indices.one_hot(4);
-    println!("{}", one_hot.to_data());
-    // [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+    let original_indices: Tensor<Backend, 1, Int> = Tensor::from_ints([0, 1, 2, 3], &device);
+    println!("original_indices: {}", original_indices);
+    // [0, 1, 2, 3]
 
-    // one hot from tensor/api/float.rs
-    let one_hot = Tensor::<Backend, 1>::one_hot(2, 10, &device);
-    println!("{}", one_hot.to_data());
+    // 本来動くべきコード torch.nn.functional.one_hotに近い。
+    // let one_hot = original_indices.one_hot(4);
+    // println!("{}", one_hot.to_data());
+    // [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+    // one_hotの中身を持ってきて、検証する。
+    let [num_samples] = original_indices.dims();
+    let indices: Tensor<Backend, 2, Int> = original_indices.unsqueeze();
+    let values: Tensor<Backend, 2, Int> = indices.ones_like();
+    let zeros: Tensor<Backend, 2, Int> =
+        Tensor::zeros([num_samples, num_samples], &indices.device());
+    let r = zeros.scatter(1, indices, values);
+    println!("{}", r.to_data());
+
+    // one hot from tensor/api/float.rs こっちは動く。
+    // let one_hot = Tensor::<Backend, 1>::one_hot(2, 10, &device);
+    // println!("{}", one_hot.to_data());
     // [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 }
